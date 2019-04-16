f0b2eccd6f53813c25bb98bfbe16269028fb2490
TheDerpGamer <garret2kool@gmail.com>
2019-04-16 11:32:42 -0700

    Update KillMobGroupQuest.java

--- src/main/java/net/thederpgamerx/fantasyhorses/quests/KillMobGroupQuest.java
+++ src/main/java/net/thederpgamerx/fantasyhorses/quests/KillMobGroupQuest.java
@@ -2,6 +2,7 @@ package net.thederpgamerx.fantasyhorses.quests;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 
 import me.blackvein.quests.Quest;
 import net.citizensnpcs.api.npc.NPC;
@@ -18,15 +19,23 @@ public class KillMobGroupQuest extends Quest {
 
 	private static Location calculateMobSpawn(Location npcLocation) {
 		World world = npcLocation.getWorld();
-		int maxDistX = 50;
-		int maxDistZ = 50;
-		int maxDistY = 15;
+		int maxDistX = 100;
+		int minDistX = -100;
+		int maxDistZ = 100;
+		int minDistZ = -100;
+        int xrange = maxDistX - minDistX + 1;
+        int zrange = maxDistZ - minDistZ + 1;
 		int npcX = npcLocation.getBlockX();
 		int npcZ = npcLocation.getBlockZ();
-		int npcY = npcLocation.getBlockY();
 		boolean goodLoc = false;
 		while(goodLoc = false) {
-			
+	        int x = (int)(Math.random() * xrange) + minDistX;
+	        int z = (int)(Math.random() * zrange) + minDistZ;
+	        int locX = npcLocation.getBlockX() - x;
+	        int locZ = npcLocation.getBlockZ() - z;
+	        int locY = world.getHighestBlockYAt(locX, locZ) + 1;
+	        Block testBlock = world.getBlockAt(locX, locY, locZ);
+	        
 		}
 		
 		return mobSpawnLocation;
