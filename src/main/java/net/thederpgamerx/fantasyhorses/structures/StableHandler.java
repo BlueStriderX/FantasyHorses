950264760e502dfb9ad110d528a09bb618bf014a
TheDerpGamer <garret2kool@gmail.com>
2019-04-18 08:19:04 -0700

    Update StableHandler.java

--- src/main/java/net/thederpgamerx/fantasyhorses/structures/StableHandler.java
+++ src/main/java/net/thederpgamerx/fantasyhorses/structures/StableHandler.java
@@ -6,15 +6,20 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
+import net.thederpgamerx.fantasyhorses.main.Main;
 
 public class StableHandler implements Listener {
 
-	public static ArrayList<NPC> NPCs = new ArrayList <NPC>();
+	public ArrayList<NPC> NPCs = new ArrayList <NPC>();
 	public static void generate(Location location, String stableType) {
 		if(stableType == "plainsStable") {
 			NPC stableKeeper = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "StableKeeper");
-			Location stableKeepLoc = null;
+			int x = location.getBlockX();
+			int y = location.getBlockY();
+			int z = location.getBlockZ();
+			Location stableKeepLoc = location.add(x, y, z);
 			stableKeeper.spawn(stableKeepLoc);
+			Main.stableKeepers.add(stableKeeper.getEntity());
 			
 		} else if(stableType == "desertStable") {
 		
