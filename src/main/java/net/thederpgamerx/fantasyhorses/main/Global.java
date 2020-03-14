0f6abdff0a6f898f2469b9f3a2280b0defed88c6
TheDerpGamer <garret2kool@gmail.com>
2020-03-14 10:36:31 -0700

    Update Global.java

--- src/main/java/net/thederpgamerx/fantasyhorses/main/Global.java
+++ src/main/java/net/thederpgamerx/fantasyhorses/main/Global.java
@@ -1,4 +1,9 @@
 package net.thederpgamerx.fantasyhorses.main;
 
+import net.thederpgamerx.fantasyhorses.horses.FantasyHorse;
+import org.bukkit.entity.Player;
+import java.util.Map;
+
 public class Global {
+    public static Map<Player, FantasyHorse> horseMap;
 }
