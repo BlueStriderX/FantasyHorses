a35a5bbe08616edc2ba9356434457d34ed7c5395
TheDerpGamer <garret2kool@gmail.com>
2020-03-16 10:57:11 -0700

    Update Utilities.java

--- src/main/java/net/thederpgamerx/fantasyhorses/main/Utilities.java
+++ src/main/java/net/thederpgamerx/fantasyhorses/main/Utilities.java
@@ -3,6 +3,10 @@ package net.thederpgamerx.fantasyhorses.main;
 import net.thederpgamerx.fantasyhorses.horses.FantasyHorse;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -12,15 +16,11 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
 
-public class FileHandler {
+public class Utilities {
 
 	private File playerFile;
 
 	public List<FantasyHorse> getHorses(Player player) {
-		/**
-		 * Gets a list of horses for a specific Player.
-		 */
-
 		List<FantasyHorse> fHorses = null;
 		UUID playerUUID = player.getUniqueId();
 		String playerDataPath = Main.getPlugin().getDataFolder() + "/horses/" + playerUUID + ".yml";
@@ -37,21 +37,33 @@ public class FileHandler {
 			int level = Integer.parseInt(horseData.get(2));
 			float health = Float.parseFloat(horseData.get(3));
 			float maxHealth = Float.parseFloat(horseData.get(4));
-			float speed = Float.parseFloat(horseData.get(5));
-			float jump = Float.parseFloat(horseData.get(6));
-			String style = horseData.get(7);
-			String color = horseData.get(8);
-			String worldString = horseData.get(9);
-			String locString = horseData.get(10);
-			Scanner scan = new Scanner(locString);
-			int x = scan.nextInt();
-			scan.skip(",");
-			int y = scan.nextInt();
-			scan.skip(",");
-			int z = scan.nextInt();
-			Location location = new Location(Bukkit.getWorld(worldString), x, y, z);
+			float armor = Float.parseFloat(horseData.get(5));
+			float speed = Float.parseFloat(horseData.get(6));
+			float jump = Float.parseFloat(horseData.get(7));
+			String styleString = horseData.get(8);
+			String colorString = horseData.get(9);
 
-			fHorses.add(new FantasyHorse(uuid, playerUUID, player, maxHealth, health, style, color, location, level, speed, jump));
+			Horse horseEntity = null;
+			Boolean horseFound = false;
+
+			for(World world : Bukkit.getWorlds()) {
+				for(Entity entity : world.getEntities()) {
+					if(entity.getType() == EntityType.HORSE && entity.getUniqueId() == uuid) {
+						horseFound = true;
+						horseEntity = (Horse) entity;
+						break;
+					}
+				}
+				break;
+			}
+
+			if(!horseFound) {
+				System.out.println("[FantasyHorses]: Tried to get a player's horse but the horse was not found on server! Marking the horse as killed...");
+				//deleteHorseFromConfig(player, l);
+			} else if (horseFound){
+				FantasyHorse fantasyHorse = new FantasyHorse(player, horseEntity, name);
+				fHorses.add(fantasyHorse);
+			}
 		}
 		return fHorses;
 	}
@@ -76,48 +88,7 @@ public class FileHandler {
 		}
 	}
 
-	/*
-	public static void saveConfigs() {
-		try {
+	public static void createConfigs() {
 
-			File playerFile = new File(Main.getPlugin().getDataFolder(), "horses.yml");
-			if(!(horsesFile.exists())) {
-				horsesFile.getParentFile().mkdirs();
-				saveResource("horses.yml", false);
-			}
-
-			horsesConfig = new YamlConfiguration();
-
-			try {
-				horsesConfig.load(horsesFile);
-
-			} catch (IOException | InvalidConfigurationException e) {
-				e.printStackTrace();
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("[FantasyHorses]: Encountered an error while trying to save configs!");
-			System.out.println("[FantasyHorses]: Disabling...");
-			Main.getPlugin().getPluginLoader().disablePlugin(Main.getPlugin());
-		}
 	}
-
-	 */
-
-		/*
-		for(String key : horsesConfig.getKeys(false)) {
-			for(World world : Bukkit.getWorlds()) {
-				for (Entity entity : world.getEntities()) {
-					if(entity.getType().equals(EntityType.HORSE)) {
-						for(int x = 0; x < horsesConfig.getStringList(playerUUID.toString()).size(); x ++) {
-							String horseUUID = horsesConfig.getStringList(playerUUID.toString()).get(x);
-							if(entity.getUniqueId().toString() == horseUUID) {
-								String playerUUID = horsesConfig.get;
-								Player player = Bukkit.getPlayer(UUID.fromString(playerUUID));
-								Global.horseMap.put()
-							}
-						}
-
-		 */
 }
